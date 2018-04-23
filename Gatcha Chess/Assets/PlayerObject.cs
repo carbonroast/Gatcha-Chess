2203ce716a5974b024c3239fa49cf01bc8ed93d9
Yangarang <jonyang7777@gmail.com>
2018-04-23 19:57:17 -0400

    Code Drop
    Lobby Tutorial Code

---
+++ Gatcha Chess/Assets/PlayerObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // Player object is invisible and not part of the world
+        // give me something physical to move around
+        Debug.Log("PlayerObject::Start -- Spawning my own personal player unit");
+        Instantiate(PlayerUnitPrefab);
+	}
+
+    public GameObject PlayerUnitPrefab;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
