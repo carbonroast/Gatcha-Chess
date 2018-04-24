923923ba1c0246c0773e37ed669083943f8cbef5
Yangarang <jonyang7777@gmail.com>
2018-04-23 21:09:49 -0400

    0.1
    code drop of merge

---
+++ New Unity Project/Assets/Scripts/King.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class King : ChessPiece {
+
+	// Use this for initialization
+	public override void Start () {
+		string ID = GetComponent<NetworkIdentity> ().netId.ToString();
+		this.transform.name = "King " + ID;
+		base.Start ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void Movement ()
+	{
+		base.Movement ();
+	}
+}
